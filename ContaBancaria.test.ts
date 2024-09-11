8bd2302c186218b18665b8510216aec7333d25f1
Jesiel Viana <jesielviana@proton.me>
2024-09-11 14:32:25 -0300

    código com erro nos testes

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,4 +1,16 @@
-// import { beforeEach, describe, expect, test } from "bun:test";
-// import ContaBancaria from "./ContaBancaria.ts";
+import { describe, expect, test } from "bun:test";
+import ContaBancaria from "./ContaBancaria.ts";
 
+describe("Testando classe ContaBancaria", () => {
+  test("Testando depositar valor positivo", () => {
+    const conta = new ContaBancaria();
+    conta.depositar(100);
+    expect(conta.consultarSaldo()).toBe(100);
+  });
 
+  test("Testando depositar valor negativo", () => {
+    const conta = new ContaBancaria();
+    conta.depositar(-100);
+    expect(conta.consultarSaldo()).toBe(10);
+  });
+});
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,13 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+  private saldo = 0;
+
+  public depositar(valor: number) {
+    if (valor > 0) {
+      this.saldo += valor;
+    }
+  }
+
+  public consultarSaldo() {
+    return this.saldo;
+  }
+}
