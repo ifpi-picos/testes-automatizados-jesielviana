5c76c3d5b5bfa5bd7b8f9a1542cf248507e1a195
Jesiel Viana <jesielviana@proton.me>
2024-09-11 14:38:00 -0300

    código sem erros nos testes

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,15 +1,18 @@
-import { describe, expect, test } from "bun:test";
+import { beforeEach, describe, expect, test } from "bun:test";
 import ContaBancaria from "./ContaBancaria.ts";
 
 describe("Testando classe ContaBancaria", () => {
+  let conta: ContaBancaria;
+  beforeEach(() => {
+    conta = new ContaBancaria();
+  });
+
   test("Testando depositar valor positivo", () => {
-    const conta = new ContaBancaria();
     conta.depositar(100);
     expect(conta.consultarSaldo()).toBe(100);
   });
 
   test("Testando depositar valor negativo", () => {
-    const conta = new ContaBancaria();
     conta.depositar(-100);
     expect(conta.consultarSaldo()).toBe(0);
   });
